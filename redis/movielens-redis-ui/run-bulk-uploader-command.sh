dc4aa7c89f07a6b3288b30c59c71fc315fd99279
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-09 16:51:09 +0530

    update redis commands for latest version

--- redis/movielens-redis-ui/run-bulk-uploader-command.sh
+++ redis/movielens-redis-ui/run-bulk-uploader-command.sh
@@ -1,10 +1,10 @@
-java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=~/rdbms_2_nosql/dasets/movielens/movielens/users.csv -type=user -truncate=true
+java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=/Users/prakashkumar/repo/rdbms_2_nosql/datasets/movielens/movielens/users.csv -type=user -truncate=true
 
 
-java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=~/rdbms_2_nosql/datasets/movielens/movielens/movies.csv -type=movie -truncate=true
+java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=/Users/prakashkumar/repo/rdbms_2_nosql/datasets/movielens/movielens/movies.csv -type=movie -truncate=true
 
 
-java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=~/rdbms_2_nosql/datasets/movielens/movielens/ratings.csv -type=rating -truncate=true
+java -cp target/movielens-redis-ui-1.0-SNAPSHOT.jar com.okmich.rdbmstonosql.redis.upload.BulkUploader -file=/Users/prakashkumar/repo/rdbms_2_nosql/datasets/movielens/movielens/ratings.csv -type=rating -truncate=true
 
 ##when using a remote redsi remember to add the arguments -host= and -port=
 
